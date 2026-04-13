93d9f30653924b051bcdce442194b5264900b8d7
Mohit Ghansela <68759832+mohitghansela@users.noreply.github.com>
2026-04-13 23:56:11 +0530

    Add files via upload

---
+++ agentic-ai-journey/day-01-llm-basics/server.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Body
+from ollama import Client
+
+app = FastAPI()
+
+client = Client(host="http://localhost:11434")
+
+@app.post("/chat")
+def chat(
+    message: str = Body(..., description="The Message")
+):
+    response = client.chat(
+        model="gemma2",
+        messages=[
+            {"role": "user", "content": message}
+        ]
+    )
+
+    return {"response": response["message"]["content"]}
